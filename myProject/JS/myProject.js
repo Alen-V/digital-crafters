138bfda85443a2f74fd2727797644a09d0b4d273
makishima98 <54191137+makishima98@users.noreply.github.com>
2020-03-29 00:41:05 +0100

    update

--- myProject/JS/myProject.js
+++ myProject/JS/myProject.js
@@ -1,5 +1,18 @@
 console.log("Not empty")
 
-if (window.outerWidth <= 1440) {
-    
+let myBtn = document.getElementById(`navbarBtn`);
+let hiddenContent = document.getElementById(`navbarHiddenContent`);
+
+function show$HideContent(content, button) {
+    if (content.style.display === `block`) {
+        content.style.display = `none`;
+        // button.style.display = `block`
+    } else {
+        content.style.display = `block`;
+        // button.style.display = `none`
+    }// add animations for the content
 }
+
+myBtn.addEventListener(`click`, function() {
+    show$HideContent(hiddenContent, myBtn)
+})
