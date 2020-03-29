2a46bf39519837b0767c2a478ccbbf02fa7240ae
makishima98 <54191137+makishima98@users.noreply.github.com>
2020-03-29 22:55:36 +0200

    transitionNavbar

--- myProject/JS/myProject.js
+++ myProject/JS/myProject.js
@@ -4,18 +4,11 @@ let myBtn = document.getElementById(`navbarBtn`);
 let hiddenContent = document.getElementById(`navbarHiddenContent`);
 let escapeBtn = document.getElementById(`collapseEscapeButton`);
 
-function show$HideContent(content) {
-    if (content.style.display === `block`) {
-        content.style.display = `none`;
-    } else {
-        content.style.display = `block`;
-    }
-}
-
 myBtn.addEventListener(`click`, function() {
-    show$HideContent(hiddenContent)
+    hiddenContent.style.width = `100%`
 })
 
 escapeBtn.addEventListener(`click`, function(){
-    show$HideContent(hiddenContent)
+    hiddenContent.style.width = `0%`
+
 })
