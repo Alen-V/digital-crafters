d8b0210e408f68c2ee1b219afe4a04aca204a2e6
makishima98 <54191137+makishima98@users.noreply.github.com>
2020-03-29 22:04:56 +0200

    collapseContent

--- myProject/JS/myProject.js
+++ myProject/JS/myProject.js
@@ -2,17 +2,20 @@ console.log("Not empty")
 
 let myBtn = document.getElementById(`navbarBtn`);
 let hiddenContent = document.getElementById(`navbarHiddenContent`);
+let escapeBtn = document.getElementById(`collapseEscapeButton`);
 
-function show$HideContent(content, button) {
+function show$HideContent(content) {
     if (content.style.display === `block`) {
         content.style.display = `none`;
-        // button.style.display = `block`
     } else {
         content.style.display = `block`;
-        // button.style.display = `none`
-    }// add animations for the content
+    }
 }
 
 myBtn.addEventListener(`click`, function() {
-    show$HideContent(hiddenContent, myBtn)
+    show$HideContent(hiddenContent)
+})
+
+escapeBtn.addEventListener(`click`, function(){
+    show$HideContent(hiddenContent)
 })
